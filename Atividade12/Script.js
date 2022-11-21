4a91a27b14fc72b5be21b95dcfb07997aa9ddda1
João Vitor dos Reis Oliveira <82333044+joaovitor889@users.noreply.github.com>
2022-11-21 12:23:08 -0300

    Add files via upload

---
+++ Atividade12/Script.js
@@ -0,0 +1,10 @@
+function abrirJanela(obj){
+    obj.src = "./janelaaberta.png"
+}
+function quebrar (obj){
+    obj.src = "./janelaquebra.png"
+}
+function janelaFechada(obj){
+    obj.src = "./janelafechada.png"
+}
+
